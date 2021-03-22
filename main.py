25d4acea4e57f516dbdfb2c484dfd5ab1fe87e58
levelfourtwenty <level420@protonmail.com>
2021-03-22 18:20:01 +0000

    Added fix to minimize irrelavent error messages

--- main.py
+++ main.py
@@ -40,29 +40,32 @@ async def on_ready():
 @RateLimiter(max_calls=7, period=23)
 @bot.event
 async def on_message(message):
-        userInput = (message.content)
-        if message.author == bot.user:
-                return
-        if userInput in misc:
-                foo = ['hmm', 'ok', 'alright' ]
-                message.channel(random.choice(foo))
-        if userInput in illegal_character:
-                print(f"{message.author} has said an illegal character")
+         try:
+                userInput = (message.content)
+                if message.author == bot.user:
+                         return
+                if userInput in misc:
+                         foo = ['hmm', 'ok', 'alright' ]
+                         message.channel(random.choice(foo))
+                if userInput in illegal_character:
+                         print(f"{message.author} has said an illegal character")
+                         return
                 # filtering characters that seem to break the script
-        if len(userInput) < 4:
-                print(f"string too short")
+                if len(userInput) < 4:
+                         print(f"string too short")
                 # adding minimum message length to try and minimize spam and language confusion
-        else:
-                sex = random.uniform(0.4, 2.5)
-                await asyncio.sleep(sex)
+                else:
+                         sex = random.uniform(0.4, 2.5)
+                         await asyncio.sleep(sex)
                 # this also helps minimize spam
-                async with message.channel.typing():
-                        response = cb.single_exchange(userInput)
+                         async with message.channel.typing():
+                                 response = cb.single_exchange(userInput)
                         # getting cleverbot ai response
-                        await message.channel.send(response)
+                                 await message.channel.send(response)
                         # sending response
-                        print(message.content)
-                        print("bot:" + response)
-
+                                 print(message.content)
+                                 print("bot:" + response)
+         except discord.errors.Forbidden:
+                   return        
 
 bot.run(token, bot=False)
